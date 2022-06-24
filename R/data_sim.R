baa07b3b4cbbbc1ad4be6316428c780a256ed5e4
peterhalpin <peter.halpin@unc.edu>
2022-06-23 22:19:18 -0400

    working

---
+++ R/data_sim.R
@@ -0,0 +1,193 @@
+require(mirt)
+require(GPCMlasso)
+require(difR)
+
+# -------------------------------------------------------------------
+#' Apply bias to a random subset of item intercepts
+#'
+#' @param b0 item difficult (not intercept) in reference group
+#' @param n.biased number of items to bias
+#' @param max.bias maximum amount of bias to apply
+#' @param worst.case logical: apply worst case bias? Default is \code{TRUE}. If set to \code{FALSE}, bias is sampled from \code{runif(max.bias/2, max.bias)}
+#'
+#' @return A vector of item intercepts, with bias added
+#'
+# -------------------------------------------------------------------
+
+apply_bias <- function(b0, n.biased = 1, max.bias = .5, worst.case = T){
+  n.items <- length(b0)
+  biased.items <- sample(n.items, n.biased)
+  bias.vector <- 0 * b0
+  if (worst.case) {
+    bias.vector[biased.items] <- max.bias
+  } else {
+    bias.vector[biased.items] <- runif(n.biased, max.bias/2, max.bias)
+  }
+  b0 + bias.vector
+}
+
+# -------------------------------------------------------------------
+#' Wrapper for mirt's implementation of the likelihood ratio (LR) test of DIF
+#'
+#' @param dat0 item response data for reference group
+#' @param dat1 item response data for comparison group
+#' @param parms which parameters to tests for DIF
+#'
+#' @return A named list with p values from the LR test and the scale parameters estimated in the model that assumes invariance
+# -------------------------------------------------------------------
+
+lr <- function(dat0, dat1, parms = c("d")){
+  dat <- rbind(dat0, dat1)
+  group <- c(rep("0", nrow(dat0)), rep("1", nrow(dat1)))
+  mg.mod <- multipleGroup(dat,
+              model = 1,
+              group = group,
+              invariance = c("intercepts",  "free_means"))
+  lr.test <- DIF(mg.mod, which.par = parms, scheme = "drop", Wald = F)
+
+  #p2 <- DIF(mg.mod, which.par = parms, scheme = "drop_sequential",
+  #          seq_stat = .05, max_run = 2, p.adjust = "BH", Wald = F)
+
+  list(p = lr.test$p, scale.parms = coef(mg.mod)$`1`$GroupPars)
+}
+
+# -------------------------------------------------------------------
+#' Wrapper for difR's implementation of the Mantel-Haenszel test of (uniform) DIF
+#'
+#' @param dat0 item response data for reference group
+#' @param dat1 item response data for comparison group
+#'
+#' @return A named list with p values from the MH test, and the scale parameter estimated from the total scores
+#'
+# -------------------------------------------------------------------
+
+mh <- function(dat0, dat1){
+  dat <- rbind(dat0, dat1)
+  group <- c(rep("0", nrow(dat0)), rep("1", nrow(dat1)))
+  p <- difR::difMH(dat, group, focal.name = 0)$p.value
+  mu <- mean(apply(dat1, 1, sum) - apply(dat0, 1, sum)) / sd(apply(dat1, 1, sum))
+  list(p = p, mu = mu)
+}
+
+# -------------------------------------------------------------------
+#' Wrapper for GPMClasso
+#'
+#' @param dat0 item response data for reference group
+#' @param dat1 item response data for comparison group
+#'
+#' @return A named list with dif parameters for the item difficulties, and the estimated mean of .
+#'
+# -------------------------------------------------------------------
+
+lasso <- function(dat0, dat1) {
+  n.items <- ncol(dat0)
+  dat <- data.frame(rbind(dat0, dat1))
+  group <- c(rep(0, nrow(dat0)), rep(1, nrow(dat1)))
+  dat.g <- data.frame(dat, group)
+  mod <- as.formula(paste("cbind(",
+                    paste(colnames(dat.g)[1:n.items],collapse=","),") ~ group"))
+  gpcm <- GPCMlasso(mod, dat.g, model = "2PL",
+                    control = ctrl_GPCMlasso(l.lambda = 20,
+                                             adaptive = T,
+                                             steptol = 1e-4,
+                                             gradtol = 1e-4,
+                                             iterlim = 50))
+  mu <- coef(gpcm)[which.min(gpcm$BIC), ][(n.items+1)]
+  dif <- abs(coef(gpcm)[which.min(gpcm$BIC), ][(n.items+2):(2*n.items+1)])
+  list(dif = dif, mu = mu)
+}
+
+
+# -------------------------------------------------------------------
+#' Runs a simulation study comparing R-DIF, the likelihood ratio test, the Mantel-Haenszel test, and the output of GPMClasso
+#'
+#' @param n.reps number of replications
+#' @param n.persons number of respondents per group
+#' @param n.item number of items
+#' @param n.biased number of items with DIF on item intercepts
+#' @param bias magnitude of bias (the same for all items with bias)
+#' @param impact the mean and variance of the latent trait in the reference groups
+#' @export
+#' @return A n.reps length list, where each element is a list containing (a) a data.frame with the output of the DIF procedures, and (b) a vector with estimates of impact from the procedures.
+#'
+# -------------------------------------------------------------------
+
+sim_study <- function(n.reps = 100, n.persons = 500, n.items = 15, n.biased = 0, bias = 0, impact = c(0, 1)){
+
+  # Item hyper-parms
+  a.lower <- .8
+  a.upper <- 2
+  b.lim <- 1.5
+
+  # Sim loop for parallelization via mclapply
+  loop <- function(i){
+
+    # DGP
+    a0 <- runif(n.items, a.lower, a.upper)
+    b0 <- sort(runif(n.items, -b.lim, b.lim))
+    b1 <- apply_bias(b0, n.biased, bias)
+    d0 <- b0*a0
+    d1 <- b1*a0
+    x0 <- rnorm(n.persons)
+    x1 <- impact[2]*rnorm(n.persons) + impact[1]
+
+    # Data gen
+    dat0 <- simdata(a0, d0, n.persons, '2PL', Theta = matrix(x0))
+    dat1 <- simdata(a0, d1, n.persons, '2PL', Theta = matrix(x1))
+
+    # Fit IRT models
+    fit0 <- mirt(dat0, 1, SE = T, SE.type = 'Oakes')
+    fit1 <- mirt(dat1, 1, SE = T, SE.type = 'Oakes')
+
+    # DIF Procedures
+    irt.mle <- get_irt_mle(list(fit0, fit1))
+    (bsq.out <- irls(irt.mle))
+    true.out <- bsq_weight(impact[1], y_fun(irt.mle), var_y(impact[1], irt.mle))
+    lr.out <- lr(dat0, dat1)
+    mh.out <- mh(dat0, dat1)
+    lasso.out <- lasso(dat0, dat1)
+
+    # Format output
+    dif <- data.frame(a0 = a0,
+                      d0 = d0,
+                      dgp = d1-d0 != 0,
+                      rdif.true = true.out,
+                      rdif = bsq.out$weights,
+                      lr = lr.out$p,
+                      mh = mh.out$p,
+                      lasso = lasso.out$dif
+                      )
+
+    scale <- data.frame(bsq = bsq.out$theta,
+                        lr = lr.out$scale.parms[1],
+                        mh = mh.out$mu,
+                        lasso = lasso.out$mu
+                        )
+
+    list(dif = dif, scale = scale, irt.mle = irt.mle)
+  }
+  parallel::mclapply(1:n.reps, loop)
+}
+
+# -------------------------------------------------------------------
+#' Post processing for \code{sim_study} to compute decision errors
+#'
+#' @param ds the DIF table output from  \code{sim_study}
+#' @export
+#' @return A data.frame with false and true positives for each method length
+#'
+# -------------------------------------------------------------------
+
+
+decision_errors <-function(ds){
+  test_names <- c("rdif.true", "rdif", "lr", "mh", "lasso")
+  cuts <- c(1e-6, 1e-6, .05, .05, 1e-6)
+  decisions <- data.frame(t(t(ds[,test_names]) < cuts))
+  decisions$lasso <- decisions$lasso == F
+  tp <- apply(decisions[ds$dgp == T, ], 2, mean)
+  fp <- apply(decisions[ds$dgp == F, ], 2, mean)
+  out <- data.frame(cbind(fp, tp))
+  out$method <- rownames(out)
+  rownames(out) <- NULL
+  out
+}
---
+++ R/robustDIF_functions.R
@@ -0,0 +1,328 @@
+# Note drop suffix 1 from functions
+# switch a0 and a1 in y_fun and grad_y
+
+#-------------------------------------------------------------------
+#' Extract and format 2PL item parms from mirt
+#'
+#' @param mirt.fit.2pl a mirt object (SingleGroupClass) for the 2PL model
+#' @return a n.items X 2 data.frame of 2PL item parameter MLEs
+#' @export
+# -------------------------------------------------------------------
+get_mirt_mle <- function(mirt.fit.2pl ){
+ n.items <- mirt.fit.2pl@Data$nitems
+ parms <- Reduce(rbind, coef(mirt.fit.2pl, printSE = T)[1:(n.items)])[, 1:2]
+ parms <- parms[row.names(parms) == "par", ]
+ parms <- data.frame(parms)
+ names(parms) <- c("a", "d")
+ parms
+}
+
+# -------------------------------------------------------------------
+#' Extract and format 2PL VCOV from mirt
+#'
+#' @param mirt.fit.2pl a mirt object (SingleGroupClass) for the 2PL model
+#' @return a n.items X n.items covariance matrix for item parms
+#' @export
+# -------------------------------------------------------------------
+
+get_mirt_vcov <- function(mirt.fit.2pl) {
+  v <- vcov(mirt.fit.2pl)
+  row.names(v) <- paste0(c("a", "d"), rep(1:(nrow(v)/2), each = 2))
+  colnames(v) <- row.names(v)
+  v
+}
+
+# -------------------------------------------------------------------
+#' Extract and format 2PL MLEs and their asymptotic VCOV matrix
+#'
+#' Takes a list of 2PL model fits and formats the MLEs and their VCOV matrix for use with \code{robustDIF}. Current implementation only supports lists of length 2 (i.e., two groups) and the first fit is treated as the reference group. The only type of fit currently supported is the \code{SingleGroupClass} of the \code{mirt} package. To use fits from other software, the user can manually format the MLEs and VCOVs in the same format as this function.
+#'
+#' @param fit.list a list of model fits.
+#' @param type a string indicating the package that produced the fits
+#' @return a named list of MLEs and VCOVs
+#' @export
+# -------------------------------------------------------------------
+
+get_irt_mle <- function(fit.list, type = "mirt") {
+ if (type == "mirt") {
+  est <- lapply(fit.list, get_mirt_mle)
+  v <- lapply(fit.list, get_mirt_vcov)
+  out <- list(par0 = est[[1]], par1 = est[[2]],
+              vcov0 = v[[1]], vcov1 = v[[2]])
+  return(out)
+ }
+}
+
+# -------------------------------------------------------------------
+#' Computes Y = (d1 - d0)/a1
+#'
+#' @param irt.mle the output of \code{robustDIF::get_irt_mle}
+#' @return vector of values Y
+#' @export
+# -------------------------------------------------------------------
+
+y_fun <- function(irt.mle) {
+  (irt.mle$par1$d - irt.mle$par0$d) / irt.mle$par1$a
+}
+
+# -------------------------------------------------------------------
+#' Compute the gradient of y, for each item
+#'
+#' For each item, the non-null elements of the gradient are organized as \code{c(a0, d0, a1, d1)}. The parameters a0 and d0 are the item slope and intercept in the reference group. The parameters a1 and d1 are the item slope and intercept in the comparison group.
+#'
+#' @param theta IRT scale parameter (mu / sigma)
+#' @param irt.mle the output of \code{robustDIF::get_irt_mle}
+#' @return A matrix in which the columns are the 4*n.items-dimensional gradient vector of y for each item
+#' @export
+# -------------------------------------------------------------------
+
+grad_y <- function(theta, irt.mle) {
+  a1 <- irt.mle$par1$a
+  n.items <- length(a1)
+  delta.a0 <- 0 / a1
+  delta.d0 <- - 1 / a1
+  delta.a1 <- - theta / a1
+  delta.d1 <- 1 / a1
+  delta <- c(rbind(delta.a0, delta.d0, delta.a1, delta.d1))
+  Delta <- matrix(delta, nrow = 4*n.items, ncol = n.items)
+  flag <- c(rep(1, times = 4), rep(0, times = 4*n.items))
+  Flag <- matrix(rep(flag, times = n.items)[1:(4*n.items^2)],
+                   nrow = 4*n.items, ncol = n.items)
+  Delta*Flag
+}
+
+# -------------------------------------------------------------------
+#' Merges the VCOV matrices of two 2PL models into a single block diagonal VCOV matrix that is conformable with the output of the \code{robustDIF} gradient functions (\code{grad_y}, \code{grad_z})
+#'
+#' @param irt.mle the output of \code{robustDIF::get_irt_mle}
+#' @return a block diagonal VCOV matrix in which each block is the VCOV of an item
+#' @export
+# -------------------------------------------------------------------
+
+joint_vcov <- function(irt.mle) {
+  n.items <- nrow(irt.mle$par0)
+  m <- diag(1:n.items) %x% matrix(1, 2, 2)
+  v0 <- lapply(split(irt.mle$vcov0, m)[-1], matrix, 2)
+  v1 <- lapply(split(irt.mle$vcov1, m)[-1], matrix, 2)
+  vfull <- vector("list", n.items * 2)
+  vfull[1:(n.items) * 2 - 1] <- v0
+  vfull[1:(n.items) * 2 ] <- v1
+  Matrix::bdiag(vfull)
+}
+
+# -------------------------------------------------------------------
+#' Computes the variance of asymptotic null distribution of y, for each item
+#'
+#' @param theta IRT scale parameter (mu / sigma)
+#' @param irt.mle the output of \code{robustDIF::get_irt_mle}
+#' @return an n.items-dimensional vector that contains the variance of y for each item
+#' @export
+# -------------------------------------------------------------------
+
+var_y <- function(theta, irt.mle) {
+  grad.y <- grad_y(theta, irt.mle)
+  joint.vcov <- joint_vcov(irt.mle)
+  vcov.y <- t(grad.y) %*% joint.vcov %*% grad.y
+  Matrix::diag(vcov.y)
+}
+
+# -------------------------------------------------------------------
+#' Computes the weights of the bi-square function for use with IRLS
+#'
+#' @param theta IRT scale parameter
+#' @param y the output of \code{robustDIF::y_fun}
+#' @param var.y the variance of y
+#' @param alpha the desired false positive rate for flagging items with DIF
+#' @return bi-square weights for (y - theta) / var.y, for each item
+#' @export
+# -------------------------------------------------------------------
+
+bsq_weight <- function(theta, y, var.y, alpha = .05){
+  r <- y - theta
+  var.theta <- 1/sum(1/var.y)
+  omega <- (var.y - var.theta)/var.y^2
+  k <- qnorm(1 - alpha/2, 0, sqrt(omega))
+  bsq.cuts <- var.y * k
+  w <- (1 - (r / bsq.cuts)^2)^2
+  w[abs(r) > bsq.cuts] <- 0
+  w
+}
+
+# -------------------------------------------------------------------
+#' Computes the staring values for IRLS (or NR)
+#'
+#' @param irt.mle the output of \code{robustDIF::get_irt_mle}
+#' @param drops logical: omit items if y changes substantially when changing ref group?
+#' @return a vector with elements med(Y), LTS(Y), and min Rho
+#' @export
+# -------------------------------------------------------------------
+
+get_starts <- function(irt.mle, drops = T){
+
+    y1 <- y_fun(irt.mle)
+    var.y1 <- var_y(0, irt.mle)
+
+    # again, switching the reference group
+    irt.mle2 <- irt.mle
+    names(irt.mle2)[1:2] <- names(irt.mle)[2:1]
+    y2 <- -1 * y_fun(irt.mle2)
+    var.y2 <- var_y(0, irt.mle)
+
+    # rescale by variance of latent trait
+    s <- median(y1/y2)
+    y2 <- s*y2
+    var.y2 <- s^2*var.y2
+
+    if (drops) {
+      drops  <- abs((y1 - y2) / sqrt(min(var.y1, var.y2))) < 1.5
+    } else {
+      drops <- T
+    }
+    y <- c(y1[drops], y2[drops])
+
+    # median residual
+    s1 <- median(y)
+
+    # lts with 50% of data
+    s2 <- lts(y)
+
+    # grid search for min of Rho function
+    s3 <- grid(irt.mle, drops)
+
+    c(s1, s2, s3)
+}
+
+# -------------------------------------------------------------------
+#' Compute the least trimmed squares (LTS) estimate of location
+#'
+#' @param y a vector of data points
+#' @param p is the proportion of data points to trim
+#' @return The LTS estimate of location
+#' @export
+# -------------------------------------------------------------------
+
+lts <- function(y, p = .5){
+  n <- length(y)
+  n.per.sample <- floor(n*p)
+  n.sample <- ceiling(n - n.per.sample + 1)
+  Y <- matrix(sort(y), nrow = n, ncol = n.sample)
+  flag <- c(rep(1, times = n.per.sample),
+            rep(0, times = n.sample))
+  Flag <- matrix(rep(flag, times = n.sample)[1:(n * n.sample)],
+                   nrow = n, ncol = n.sample)
+  dat <- Y * Flag
+  sum(dat[,which.min(apply(dat, 2, var))]) / n.per.sample
+}
+
+# -------------------------------------------------------------------
+#' The bi-square rho function
+#'
+#' @param u can be a single value, vector, or matrix
+#' @param k the tuning parameter of the bi-square
+#' @return The bi-square rho function
+#' @export
+# -------------------------------------------------------------------
+
+rho <- function(u, k = 1.96) {
+  w <- (u / k)^2
+  out <- k^2 / 6 * (1 - (1 - w)^3)
+  out[abs(u) > k] <- k^2 / 6
+  return(out)
+}
+
+# -------------------------------------------------------------------
+#' Performs grid search for minimum value of the bi-square Rho function
+#'
+#' @param irt.mle the output of \code{robustDIF::get_irt_mle}
+#' @param alpha the desired false positive rate for flagging items with DIF
+#' @param grid.width the width of grid points
+#' @param drops passed from \code{get_starts}; leave at default value (\code{T}) to keep all items
+#' @return The location parameter that minimizes the bi-square Rho function
+#' @export
+# -------------------------------------------------------------------
+
+grid <- function(irt.mle, alpha = .05, grid.width = .05, drops = T){
+  y1 <- y_fun(irt.mle)
+  theta <- seq(from = max(min(y1), -1.5),
+               to = min(max(y1), 1.5),
+               by = grid.width)
+
+  n.items <- length(y1)
+  n.theta <- length(theta)
+
+  Theta <- matrix(theta, nrow = n.items, ncol = n.theta, byrow = T)
+  Y1 <- matrix(y1, nrow = n.items, ncol = n.theta)
+  SE.Y1 <- Reduce(cbind, lapply(theta, function(x) sqrt(var_y(x, irt.mle))))
+  U <- Y1 - Theta / SE.Y1
+
+  # Again, switching reference group
+  irt.mle2 <- irt.mle
+  names(irt.mle2)[1:2] <- names(irt.mle)[2:1]
+  y2 <- -1 * y_fun(irt.mle2)
+  s <- median(y1 / y2)
+  Y2 <- matrix(s * y2, nrow = n.items, ncol = n.theta)
+  SE.Y2 <- Reduce(cbind, lapply(theta, function(x) s * sqrt(var_y(x, irt.mle2))))
+  U2 <- Y2 - Theta / SE.Y2
+
+  R <- apply(rho(U)*drops + rho(U2)*drops, 2, sum)
+  # plot(theta, R)
+  theta[which.min(R)]
+}
+
+# -------------------------------------------------------------------
+#' IRLS for an IRT scale parameter using the bi-square function
+#'
+#' @param irt.mle the output of \code{robustDIF::irt_mle}
+#' @param alpha the desired false positive rate for flagging items with DIF
+#' @param starting.value one of \code{c("med", "lts", "min_rho", "all")} or a numerical value to be used as the starting value. The default is \code{"all"} which returns the median of the other three.
+#' @param tol convergence criterion for comparing subsequent values of estimate
+#' @param maxit maximum number of iterations to perform
+#'
+#' @return A named list containing the estimate, weights, and number of iterations
+#' @export
+# -------------------------------------------------------------------
+
+irls <- function(irt.mle, alpha = .05, starting.value = "all", tol = 1e-5, maxit = 100){
+  nit <- 0
+  conv <- 1
+
+  # Starting value
+  starts <- get_starts(irt.mle)
+  theta <- median(starts)
+  if (starting.value == "med") {theta <- starts[1]}
+  if (starting.value == "lts") {theta <- starts[2]}
+  if (starting.value == "min_rho") {theta <- starts[3]}
+  if (is.numeric(starting.value)) {theta <- starting.value}
+  y <- y_fun(irt.mle)
+
+  # Loop
+   while(nit < maxit & conv > tol) {
+    var.y <- var_y(theta, irt.mle)
+    w <- bsq_weight(theta, y, var.y, alpha)
+    new.theta <- sum(w * y / var.y ) / sum(w / var.y)
+    nit <- nit + 1
+    conv <- abs(theta - new.theta)
+    theta <- new.theta
+  }
+  list(theta = new.theta, weights = w, n.iter = nit, epsilon = conv)
+}
+
+# -------------------------------------------------------------------
+#' R-DIF test for item intercepts
+#'
+#' @param theta IRT scale parameter
+#' @param irt.mle the output of \code{robustDIF::irt_mle}
+#' @param alpha the desired false positive rate for flagging items with DIF
+#'
+#' @return A n.items-dimensional vector of p-values
+#' @export
+# -------------------------------------------------------------------
+
+d_test <- function(theta, irt.mle, alpha = .05) {
+  y <- y_fun(irt.mle)
+  var.y <- var_y(scale, irt.mle)
+  var.theta <- 1/(sum(1/var.y))
+  crit <- qnorm(1 - alpha/2)
+  abs(y - theta) / sqrt(var.y - var.theta) > crit
+}
+
